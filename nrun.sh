be8e9b87bdb794061cd9e9e993850324b2434840
jondough <ewhimsical@gmail.com>
2024-01-04 19:58:28 -0800

    fix scripts

--- nrun.sh
+++ nrun.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# activate venv for Windows
+source bot-env/Scripts/activate
+# activate venv for Linux
+# source bot-env/bin/activate
+
 # clear nohup.out
 rm -f nohup.out
 # start bot in background
--- run.sh
+++ run.sh
@@ -1,2 +1,8 @@
 #!/bin/sh
+
+# activate venv for Windows
+source bot-env/Scripts/activate
+# activate venv for Linux
+# source bot-env/bin/activate
+
 python main.py
