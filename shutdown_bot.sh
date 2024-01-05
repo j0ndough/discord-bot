fa52a76e6c2eec629d8df21348e7c47664aa6ee8
jondough <ewhimsical@gmail.com>
2024-01-04 22:59:42 -0800

    update shutdown_bot.sh

--- shutdown_bot.sh
+++ shutdown_bot.sh
@@ -3,10 +3,10 @@
 OS="$OSTYPE" # Get OS platform
 case $OS in
     msys*) # Windows OS
-        pid=$(ps aux | grep python | awk '{print $1}')
+        pid=$(ps aux | grep python | awk '{print $1}' | head -n 1)
     ;;
     *) # Non Windows OS
-        pid=$(ps aux | grep python | awk '{print $2}')
+        pid=$(ps aux | grep python | awk '{print $2}' | head -n 1)
     ;;
 esac
 
